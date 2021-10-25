bed188f458eff252e184e32effb39782b7b49f90
Ammar A Hasan <ammar.hasan162@gmail.com>
2021-10-25 12:34:21 +0300

    ChatRoom, ChatMessage relashionship

--- app/Models/ChatRoom.php
+++ app/Models/ChatRoom.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\ChatMessage;
+
 class ChatRoom extends Model
 {
     use HasFactory;
+
+    public function messages()
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
 }
